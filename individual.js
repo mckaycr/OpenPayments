d4b65a72d5507d0b503c8e2849f2ad1df0b3cb1c
unknown <M9HC@LT55518.cms.local>
2015-09-03 09:47:29 -0400

    This is the primary module I'm working on.  This is the meat of the project.  It accepts an entity ID from open payments and returns all the summed up values for that covered recipient.
    Things to do here, sum up all the PI information related to the entity

---
+++ individual.js
@@ -0,0 +1,95 @@
+var request = require('sync-request')
+	,fs = require('fs')
+	,config = require('config');
+
+module.exports = {
+	query : function(options){
+		main(options);
+		return Covered_Recipient
+	}	 	
+};
+
+// This object will be returned with all the results from the six queries
+var Covered_Recipient = {}
+
+function main(options){
+	var datasets = buildArr();
+	var Entity_type = options.type + '_id'
+	Covered_Recipient[Entity_type] = options.id
+	datasets.forEach(function(e){
+		getResults(e[0],e[1],options.id);
+	})
+}
+
+// This function reads the config file and stores all the url information for the datasets you want to query.
+// It will return an array of where each element contians program year, payment type, and the dataset URL with no paramerters
+function buildArr(){
+	var validation_messages = config.get('datasets')
+	var temp = [];
+	for (var key in validation_messages) {
+	   if (validation_messages.hasOwnProperty(key)) {
+	       var obj = validation_messages[key];
+	        for (var prop in obj) {
+	          if(obj.hasOwnProperty(prop)){
+	            temp.push([key,prop,obj[prop]]);
+	          }
+	       }
+	    }
+	}
+	return temp
+}
+
+// This function will return a URL in the form of a string
+function buildURI(program_year,payment_type,entity_id){
+	return config.get("datasets." + program_year + "." + payment_type) + "?$$app_token="+config.get('auth.app_token')+config.get('params.physician.'+payment_type) + entity_id +"'"
+}
+
+// This function will update the global variable Covered_recipient with all the information from each query
+function getResults(program_year,payment_type,entity_id){
+	var opayment_typeions = {
+		    method: 'GET'
+		    ,uri: buildURI(program_year,payment_type,entity_id)
+		    ,gzip: true
+		    ,json: true
+		    ,auth: {
+			    user: config.get('auth.user')
+			    ,pass: config.get('auth.key')
+		       	,sendImmediately: true
+			}
+		};
+	var temp = request(opayment_typeions.method,opayment_typeions.uri,opayment_typeions).getBody('utf8')
+	var arrResults = JSON.parse(temp)
+	if(arrResults.length>0){
+		if(!(Covered_Recipient.hasOwnProperty(program_year))){Covered_Recipient[program_year]= {}}
+		if(!(Covered_Recipient[program_year].hasOwnProperty(payment_type))){Covered_Recipient[program_year][payment_type]= {}}
+
+		arrResults.forEach(function(element){
+			if(!(Covered_Recipient[program_year][payment_type].hasOwnProperty('value'))){
+				Covered_Recipient[program_year][payment_type].value = 0
+				Covered_Recipient[program_year][payment_type].count = 0
+			}
+			switch(payment_type){
+				case 'ownership':
+					if(!(Covered_Recipient[program_year][payment_type].hasOwnProperty('interest'))){Covered_Recipient[program_year][payment_type].interest = 0}
+					Covered_Recipient[program_year][payment_type].value = Covered_Recipient[program_year][payment_type].value + Number(element.sum_total_amount_invested_usdollars)
+					Covered_Recipient[program_year][payment_type].interest = Covered_Recipient[program_year][payment_type].interest + Number(element.sum_value_of_interest)
+					Covered_Recipient[program_year][payment_type].count = Covered_Recipient[program_year][payment_type].count + Number(element.count_record_id)
+					break;
+				default:
+					Covered_Recipient[program_year][payment_type].value = Covered_Recipient[program_year][payment_type].value + Number(element.sum_total_amount_of_payment_usdollars)
+					Covered_Recipient[program_year][payment_type].count = Covered_Recipient[program_year][payment_type].count + Number(element.count_record_id)
+			}
+			switch(element.dispute_status_for_publication){
+				case 'Yes':
+					if( Covered_Recipient[program_year].disputes== 'undefinded' || Covered_Recipient[program_year].disputes==null){Covered_Recipient[program_year].disputes = Number(element.count_record_id)}
+					else{Covered_Recipient[program_year].disputes = Covered_Recipient[program_year].disputes + Number(element.count_record_id)}
+					break;
+				case 'No':
+					if( Covered_Recipient[program_year].undisputes== 'undefinded' || Covered_Recipient[program_year].undisputes==null){Covered_Recipient[program_year].undisputes = Number(element.count_record_id)}
+					else{Covered_Recipient[program_year].undisputes = Covered_Recipient[program_year].undisputes + Number(element.count_record_id)}
+					break;
+			}
+		})
+	}
+}
+
