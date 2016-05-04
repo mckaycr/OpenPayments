7419311035e257b8ae67f4eeed0fb9a176914caa
mckaycr <mckaycr@gmail.com>
2016-05-04 12:27:33 -0400

    Resulting object now mirrors what the original module used to produce with the exception of disputes now being an object that contains count and value versus just being a number.  There is also no undisputes

--- test/test.js
+++ test/test.js
@@ -32,23 +32,23 @@ describe('open-payments',function(){
             var options = {'id':'268527','type': 'physician'};
             this.timeout(15000)
             cr.summary(options, function(err, data){
-                expect(data[0].data[0]).to.have.a.property('count','81')
+                expect(data[2013].general).to.have.a.property('count',81)
                 done()
             });
         });
         it('It should return data from for company from every program year and every dataset where data exists', function(done){
             var options = {'id':'100000000286','type': 'company'};
-            this.timeout(15000)
+            this.timeout(150000)
             cr.summary(options, function(err, data){
-                expect(data[0].data[0]).to.have.a.property('count','176265')
+                expect(data[2013].general).to.have.a.property('count',176273)
                 done()
             });
         });
         it('It should return data from for hospital from every program year and every dataset where data exists', function(done){
             var options = {'id':'644','type': 'hospital'};
-            this.timeout(15000)
+            this.timeout(150000)
             cr.summary(options, function(err, data){
-                expect(data[0].data[0]).to.have.a.property('count','45')
+                expect(data[2013].general).to.have.a.property('count',49)
                 done()
             });
         });
